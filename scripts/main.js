74118a988b4ebb44b8dc1a00a1fab7663316b5f8
christian <christian.amado97@gmail.com>
2025-04-18 18:40:53 -0300

    entendendo variáves

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+// window.alert('hello world!')
+// window.confirm('Confirma ai')
+// window.prompt("What's your name")
+
+// var nome = window.prompt('Whats your name');
+// window.alert('Olá, '+ nome + '!');
+
+// var n1 = Number(window.prompt('Digite um número: '))
+// var n2 = Number(window.prompt('Digite outro number: '))
+
+// var n3 = n1 + n2
+
+// window.alert(n3)
+
+// console.log(marca);
+// console.log(nulo);
+// console.log(nome);
+
+
+let nome = window.prompt('Qual o teu nome?: ')
+let sobrenome = window.prompt ('Qual o teu sobrenome?: ')
+
+window.alert('Olá sr. '+ nome + ' '+ sobrenome)
+
+let n1 = Number(window.prompt('Primeiro number: '))
+let n2 = Number(window.prompt('Segundo numero: '))
+let n3 = n1 + n2
+
+window.alert('O resultado é '+ n3)
